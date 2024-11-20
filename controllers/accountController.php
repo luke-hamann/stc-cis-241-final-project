5dcd65f576a550ef69005dc45cefa9ce65eda743
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-11-20 17:42:11 -0600

    Cleanup account controller

--- controllers/accountController.php
+++ controllers/accountController.php
@@ -1,69 +1,78 @@
 <?php
+/**
+ * Title: Account Controller
+ * Purpose: To manage user sessions, including logins, logouts, and registrations
+ */
 
 require_once('./models/loginForm.php');
 require_once('./models/registerForm.php');
 
-if ($action == 'login' && $isGet) {
-    include('./views/account/login.php');
-    exit();
-}
-
-if ($action == 'login' && $isPost) {
+/**
+ * Render the login form and process attempted logins
+ */
+if ($action == 'login') {
     if (isset($currentUser)) {
         header('Location: .');
     }
 
-    $loginForm = LoginForm::fromArray($_POST);
-
-    if (!$loginForm->isValid()) {
-        $errors = $loginForm->getErrors();
+    if ($isGet) {
         include('./views/account/login.php');
         exit();
-    }
+    } else if ($isPost) {
+        $loginForm = LoginForm::fromArray($_POST);
 
-    $user = UserDB::loginUser($loginForm->name, $loginForm->password);
-
-    if (!isset($user)) {
-        $errors = ['Invalid credentials.'];
-        include('./views/account/login.php');
-        exit();
-    }
-
-    $_SESSION['userId'] = $user->id;
-    header('Location: .');
-} 
-
-if ($action == 'register' && $isGet) {
-    if (isset($currentUser)) {
+        if (!$loginForm->isValid()) {
+            $errors = $loginForm->getErrors();
+            include('./views/account/login.php');
+            exit();
+        }
+    
+        $user = UserDB::loginUser($loginForm->name, $loginForm->password);
+    
+        if (!isset($user)) {
+            $errors = ['Invalid credentials.'];
+            include('./views/account/login.php');
+            exit();
+        }
+    
+        $_SESSION['userId'] = $user->id;
         header('Location: .');
     }
-
-    include('./views/account/register.php');
-    exit();
 }
 
-if ($action == 'register' && $isPost) {
+/**
+ * Render the registration form and process attempted registrations
+ */
+if ($action == 'register') {
     if (isset($currentUser)) {
         header('Location: .');
     }
 
-    $registerForm = RegisterForm::fromArray($_POST);
-
-    $errors = $registerForm->getErrors();
-    if (UserDB::getUserByName($registerForm->name) !== null) {
-        array_unshift($errors, 'Username is taken.');
-    }
-
-    if (count($errors) > 0) {
+    if ($isGet) {
         include('./views/account/register.php');
         exit();
-    }
+    } else if ($isPost) {
+        $registerForm = RegisterForm::fromArray($_POST);
 
-    $userId = UserDB::addUser($registerForm->getUser());
-    $_SESSION['userId'] = $userId;
-    header('Location: .');
+        $errors = $registerForm->getErrors();
+        if (UserDB::getUserByName($registerForm->name) !== null) {
+            array_unshift($errors, 'Username is taken.');
+        }
+    
+        if (count($errors) > 0) {
+            include('./views/account/register.php');
+            exit();
+        }
+    
+        $userId = UserDB::addUser($registerForm->getUser());
+        $_SESSION['userId'] = $userId;
+        header('Location: .');
+    }
 }
 
+/**
+ * Process logout attempts
+ */
 if ($action == 'logout' && $isPost) {
     $_SESSION = array();
     session_destroy();
