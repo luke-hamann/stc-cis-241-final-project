0e1d3d2ffe3861c650d2443cd9d8e1ce2460a563
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-11-29 15:35:49 -0600

    Fix edit forum view

--- views/admin/editForum.php
+++ views/admin/editForum.php
@@ -8,23 +8,22 @@ if ($model->mode == 'Add') {
 }
 ?>
 
-<h1>
-    <?php echo $model->mode; ?> Forum
-</h1>
-
-<form action="<?php echo $formAction; ?>" method="post">
-    <input type="hidden" name="id"
-        value="<?php echo (isset($model->forum) ? $model->forum->id : '0'); ?>" />
-    <?php include './views/shared/formErrors.php'; ?>
-    <div>
-        <label for="name">Name</label>
-        <input type="text" name="name" id="name"
-            value="<?php echo (isset($model->forum) ? $model->forum->name : ''); ?>" />
+<div class="row">
+    <div class="col-md-4 offset-md-4 my-5">
+        <form action="<?php echo $formAction; ?>" method="post">
+            <h1><?php echo $model->mode; ?> Forum</h1>
+            <input type="hidden" name="id" value="<?php echo (isset($model->forum) ? $model->forum->id : '0'); ?>" />
+            <?php include './views/shared/formErrors.php'; ?>
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" name="name" id="name" value="<?php echo (isset($model->forum) ? $model->forum->name : ''); ?>" class="form-control" />
+            </div>
+            <div class="text-center">
+                <input type="submit" value="Submit" class="btn btn-primary" />
+                <a href="?action=forums" class="btn btn-secondary">Cancel</a>
+            </div>
+        </form>
     </div>
-    <div>
-        <input type="submit" value="Submit" />
-        <a href="?action=forums">Cancel</a>
-    </div>
-</form>
+</div>
 
 <?php include('./views/shared/footer.php'); ?>
