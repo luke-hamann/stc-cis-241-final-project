e9b431ef45a743018244ae2decd459d1b0534301
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-11-30 20:08:31 -0600

    Patch edit forum view

--- views/admin/editForum.php
+++ views/admin/editForum.php
@@ -8,12 +8,12 @@ if ($model->mode == 'Add') {
 }
 ?>
 
-<div class="row">
+<div class="container">
     <div class="col-md-4 offset-md-4 my-5">
         <form action="<?php echo $formAction; ?>" method="post">
             <h1><?php echo $model->mode; ?> Forum</h1>
             <input type="hidden" name="id" value="<?php echo (isset($model->forum) ? $model->forum->id : '0'); ?>" />
-            <?php include './views/shared/formErrors.php'; ?>
+            <?php include('./views/shared/formErrors.php'); ?>
             <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
                 <input type="text" name="name" id="name" value="<?php echo (isset($model->forum) ? $model->forum->name : ''); ?>" class="form-control" />
