e46c169bde7c4148625c1bece4aa7b01bfc66e23
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-12-06 06:57:03 -0600

    Fix forums page layout

--- views/home/forums.php
+++ views/home/forums.php
@@ -1,30 +1,30 @@
 <?php include('./views/shared/header.php'); ?>
 
 <div class="row">
-    <h1 class="p-3 m-3">Forums</h1>
+    <h1 class="p-3 mt-3 mb-0">Forums</h1>
     <?php if (isset($model->currentUser) && $model->currentUser->isAdmin) : ?>
-        <div class="mx-3 px-3">
+        <div class="p-3">
             <a href="?action=addForum" class="btn btn-primary">&plus; Add Forum</a>
         </div>
     <?php endif; ?>
     <?php if (count($model->forums) == 0) : ?>
-        <p class="p-3 m-3">
+        <p class="p-3">
             No forums.
         </p>
     <?php else : ?>
-        <p class="p-3 m-3">
+        <p class="p-3">
             <?php echo count($model->forums) . ' forum' . (count($model->forums) == 1 ? '' : 's'); ?>
         </p>
-        <table class="m-3">
+        <table class="table table-striped p-3 mb-5">
             <tbody>
                 <?php foreach ($model->forums as $forum) : ?>
                     <tr>
                         <td>
-                            <a href="?action=forum&id=<?php echo $forum->id; ?>" class="d-block m-3 text-decoration-none"><?php echo htmlspecialchars($forum->name); ?></a>
+                            <a href="?action=forum&id=<?php echo $forum->id; ?>" class="d-block text-decoration-none"><?php echo htmlspecialchars($forum->name); ?></a>
                         </td>
                         <?php if (isset($model->currentUser) && $model->currentUser->isAdmin) : ?>
                             <td>
-                                <a href="?action=editForum&id=<?php echo $forum->id; ?>" class="btn btn-warning m-3">Edit</a>
+                                <a href="?action=editForum&id=<?php echo $forum->id; ?>" class="btn btn-warning">Edit</a>
                             </td>
                             <td>
                                 <a href="?action=deleteForum&id=<?php echo $forum->id; ?>" class="btn btn-danger">Delete</a>
