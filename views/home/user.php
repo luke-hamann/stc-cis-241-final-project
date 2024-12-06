f3a4fb1d0c8c027d6bddc04fcf9c111ae3e0a60c
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-12-06 07:14:50 -0600

    Fix user page layout

--- views/home/user.php
+++ views/home/user.php
@@ -1,35 +1,35 @@
 <?php include('./views/shared/header.php'); ?>
 
 <div class="row">
-    <div>
-        <h1 class="p-3 m-3">
-            <?php echo htmlspecialchars($model->user->name); ?>
-        </h1>
-        <?php if (isset($model->currentUser) && $model->user->id == $model->currentUser->id) : ?>
-            <div class="p-3 m-3">
-                <a href="?action=changePassword" class="btn btn-warning">Change Password</a>
-            </div>
-        <?php endif; ?>
-        <ul class="nav nav-tabs">
-            <li class="nav-item">
-                <a href="?action=user&id=<?php echo $model->user->id; ?>&mode=posts" class="nav-link <?php echo ($model->mode == 'posts' ? 'active' : ''); ?>">Posts</a>
-            </li>
-            <li class="nav-item">
-                <a href="?action=user&id=<?php echo $model->user->id; ?>&mode=comments" class="nav-link <?php echo ($model->mode == 'comments' ? 'active' : ''); ?>">Comments</a>
-            </li>
-        </ul>
-    </div>
+    <h1 class="p-3 mt-3 mb-0">
+        <?php echo htmlspecialchars($model->user->name); ?>
+    </h1>
+    <?php if (isset($model->currentUser) && $model->user->id == $model->currentUser->id) : ?>
+        <div class="p-3 mb-3">
+            <a href="?action=changePassword" class="btn btn-warning">Change Password</a>
+        </div>
+    <?php endif; ?>
+    <ul class="nav nav-tabs my-3">
+        <li class="nav-item">
+            <a href="?action=user&id=<?php echo $model->user->id; ?>&mode=posts"
+                class="nav-link <?php echo ($model->mode == 'posts' ? 'active' : ''); ?>">Posts</a>
+        </li>
+        <li class="nav-item">
+            <a href="?action=user&id=<?php echo $model->user->id; ?>&mode=comments"
+                class="nav-link <?php echo ($model->mode == 'comments' ? 'active' : ''); ?>">Comments</a>
+        </li>
+    </ul>
     <?php if ($model->mode == 'posts') : ?>
-        <p class="m-3">
+        <div class="p-3">
             <?php echo count($model->posts) . ' post' . (count($model->posts) == 1 ? '' : 's'); ?>
-        </p>
+        </div>
         <?php foreach ($model->posts as $post) : ?>
             <?php include('./views/shared/post.php'); ?>
         <?php endforeach; ?>
     <?php elseif ($model->mode == 'comments') : ?>
-        <p class="m-3">
+        <div class="p-3">
             <?php echo count($model->comments) . ' comment' . (count($model->comments) == 1 ? '' : 's'); ?>
-        </p>
+        </div>
         <?php foreach ($model->comments as $comment) : ?>
             <?php include('./views/shared/comment.php'); ?>
         <?php endforeach; ?>
