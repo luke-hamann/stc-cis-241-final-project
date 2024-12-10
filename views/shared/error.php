1fb08769ae2e1520bde0494728371b2b694d5cec
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-12-09 20:01:36 -0600

    Fix error page layout

--- views/shared/error.php
+++ views/shared/error.php
@@ -1,10 +1,8 @@
 <?php include('views/shared/header.php'); ?>
 
-<div class="row my-5">
-    <div class="col-md-6 offset-md-3">
-        <h1><?php echo htmlspecialchars($model->title); ?></h1>
-        <div><?php echo htmlspecialchars($model->body); ?></div>
-    </div>
+<div class="row">
+    <h1 class="p-3 mt-3 mb-0"><?php echo htmlspecialchars($model->title); ?></h1>
+    <div class="p-3"><?php echo htmlspecialchars($model->body); ?></div>
 </div>
 
 <?php include('views/shared/footer.php'); ?>
