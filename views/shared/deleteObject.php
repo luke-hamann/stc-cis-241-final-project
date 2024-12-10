b6475eeb1726adde143f100291f8352fd0f4d9b3
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-12-09 19:06:34 -0600

    Refine deletion confirmation message

--- views/shared/deleteObject.php
+++ views/shared/deleteObject.php
@@ -6,7 +6,8 @@
             <h1>Confirm Deletion</h1>
             <input type="hidden" name="id" value="<?php echo $model->id; ?>" />
             <div class="mb-3">
-                Are you sure you want to delete "<?php echo $model->summary; ?>"?
+                Are you sure you want to delete "<?php
+                    echo strlen($model->summary) > 50 ? substr($model->summary, 0, 47) . '...' : $model->summary; ?>"?
             </div>
             <div class="text-center">
                 <input type="submit" value="Yes" class="btn btn-danger" />
